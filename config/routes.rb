64a60fbc356ae97d456c4ae5e7f8413c38a8c4fa
Seth Allen <seth.a@me.com>
2016-01-17 23:02:29 +0000

    Added EcoElectronix Site Data

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 LiveEco::Application.routes.draw do
+  root 'static_pages#index'
+  get 'about', :to => 'static_pages#about'
+  get "privacy", :to => "static_pages#privacy"
+  get "careers", :to => "static_pages#careers"
+  get 'ex25', :to => 'landings#ex25'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
