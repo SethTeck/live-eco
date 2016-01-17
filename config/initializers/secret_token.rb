a81dfd80a7dab7d103ed56ac0239d4abdcfd438a
Seth Allen <seth.a@me.com>
2016-01-17 22:33:12 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LiveEco::Application.config.secret_key_base = 'fc303db9c683ef10aa1da7214e909b7c8ca923e14006e1efe325a5743f76f49b70dc0c11267890a8a051a33284bc4d4a5d53b8cb24a22f36ac26be8e1e0de404'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LiveEco::Application.config.session_store :cookie_store, key: '_live-eco_session'
